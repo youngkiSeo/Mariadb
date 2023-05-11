aa13e93c6ef40d30bddd8d2da8f8edfdce28740d
youngkiSeo <130648198+youngkiSeo@users.noreply.github.com>
2023-05-11 17:24:02 +0900

    Add files via upload

---
+++ ch06.sql
@@ -0,0 +1,83 @@
+-- ch 6 05-11
+
+
+-- union 연산 A와 B의 결합, union all 중복허용.
+SELECT 1 num, 1 as str
+UNION all
+SELECT 9 num, 'xyzaa' as str
+UNION all
+SELECT 1 AS num, 3 AS str;
+
+--  고객의 이름, 성/ 배우의 이름,성 같이 띄워주세요. union 이용.
+
+SELECT * FROM customer;
+SELECT * FROM actor;
+
+SELECT 'customer' as typ,first_name, last_name FROM customer
+
+UNION
+SELECT 'actor',first_name, last_name FROM actor
+ORDER BY typ,first_name;
+
+-- 배우, 고객 둘다 이름이(first_name) J or D로 시작하는 사람 리스트.
+
+SELECT first_name, last_name from customer
+WHERE first_name REGEXP '^[JD]'
+UNION
+SELECT first_name, last_name FROM actor
+WHERE first_name REGEXP '^[JD]'
+;
+
+SELECT T.first_name, T.last_name 
+FROM(
+	SELECT  'customer' AS typ, first_name, last_name
+	FROM customer
+	union
+	SELECT 'actor',first_name, last_name 
+	FROM actor 
+)T
+ WHERE T.first_name REGEXP '^J' AND T.last_name REGEXP '^D'
+ GROUP BY T.first_name, T.last_name
+ HAVING COUNT(1)>1
+;
+
+-- except
+SELECT typ,T.first_name, T.last_name 
+FROM(
+	SELECT  'customer' AS typ, first_name, last_name
+	FROM customer
+	union
+	SELECT 'actor',first_name, last_name 
+	FROM actor 
+)T
+ WHERE T.first_name REGEXP '^J' AND T.last_name REGEXP '^D'
+ GROUP BY T.first_name, T.last_name
+ HAVING COUNT(1) = 1;
+
+-- exam
+
+CREATE TABLE except_b(
+id INT UNSIGNED);
+
+INSERT INTO except_a
+(id)
+VALUES
+(10),(11),(12),(10),(10);
+
+INSERT INTO except_b
+(id)
+VALUES
+(10),(10);
+
+-- 10, 11,12
+
+SELECT id FROM except_a
+UNION
+SELECT id FROM except_b;
+
+SELECT T.first_name, T.last_name FROM (
+SELECT first_name, last_name FROM actor
+UNION
+SELECT first_name, last_name FROM customer) T
+WHERE T.last_name REGEXP '^L'
+ORDER BY last_name0;
---
+++ ch07.sql
@@ -0,0 +1,115 @@
+-- chapter7 05-11
+
+ CREATE TABLE string_tb1 (
+ CHAR_fld CHAR(30),
+ vchar_fld varCHAR(30),
+ text_fld text
+ );
+ 
+ INSERT INTO string_tb1
+ (char_fld, vchar_fld, text_fld)
+ values
+ (
+ 	'This is char data',
+ 	'This is varchar data',
+ 	'This is text data'
+ );
+ SELECT *, QUOTE (text_fld) FROM string_tb1; 
+ SELECT * FROM string_tb1;
+ 
+ UPDATE STRING_tb1
+ SET vchar_fld = 'this is a piece of extremely long varchar data';
+  UPDATE STRING_tb1
+ SET text_fld = 'This s\'tring didn\'t WORK, but it does now';
+ 
+ SELECT @@SESSION.sql_mode;
+ 
+ SET sql_mode = 'strict';
+ 
+ SHOW WARNINGS;
+  --문자열 합치기 CONCAT
+ SELECT lname, fname, CONCAT(lname,' ',fname)
+ FROM person;
+ 
+ SELECT lname,char_LENGTH(lname)
+ FROM person; 
+ 
+ -- 문자열 찾기. 
+ SELECT lname, POSITION('터너' IN lname)
+ FROM person;
+
+SELECT * FROM string_tb1;
+SELECT TEXT_fld, POSITION('n' IN text_fld), LOCATE('n', text_fld, 12) -- 12번째 부터 찾겠다.
+FROM string_tb1;
+SELECT '안녕' = '안녕', 'abc' = 'ABC', 'abc' = 'cba', STRCMP('abc', 'ABC');
+
+SELECT NAME, name LIKE '%y',
+NAME REGEXP '^[C]'
+FROM category;
+
+
+-- mysql에서는 사용불가능 연결연산자.
+SELECT first_name || ' ' || last_name
+FROM customer;
+
+-- replace BA를 DA로 바꾸겠다!!
+SELECT first_name, REPLACE(first_name, 'BA', 'DA')FROM customer;
+
+-- 성에서 PH > TH 로 바꾸고 NI 는 NA 로바꿔라!
+SELECT customer_id , first_name, REPLACE(REPLACE(first_name,'NI','NA'), 'PH','TH' ) FROM customer;
+
+SELECT 'goodbyeworld', INSERT('goodbye world',9,5,'cruel ');
+
+-- 영화 제목 빈칸에 nice 추가해주세요
+SELECT * FROM film;
+SELECT title , INSERT(title,POSITION(' 'IN title),0, ' nice ')FROM film;
+
+SELECT title, REPLACE(title, ' ',' nice ' ) FROM film;
+
+SELECT email, SUBSTRING(email,3),SUBSTRING(email,3,6),substr(email,3,6)
+ FROM customer;
+ 
+-- 사용자 이메일 쪼개서 출력 @기준으로.
+SELECT email, 
+SUBSTRING(email, 1,POSITION( '@' IN email)-1) AS email_1, 
+SUBSTRING(email,POSITION( '@' IN email)+1) AS email_2,
+LEFT(email,POSITION( '@' IN email)-1),
+RIGHT(email, CHA0R_LENGTH(email) - POSITION( '@' IN email) )
+FROM customer;
+
+SELECT * FROM customer;
+
+SELECT (38*59) / (78- (8*6));
+
+SELECT MOD(10,7);
+
+SELECT TRUNCATE(1123.3456,2), ABS(-10), ABS(10);
+
+SELECT NOW(), CURRENT_DATE(), CURRENT_TIME(), CURRENT_TIMESTAMP();
+
+-- 형변환 CAST CONVERT
+SELECT CAST('2019/05/11 16:46:30' AS DATETIME),
+CONVERT ('2019-05-11 16:46:30', DATETIME);
+
+SELECT DATE_ADD(CURRENT_DATE(), INTERVAL 5000 DAY),
+DATE_ADD(NOW(), INTERVAL '03:27:11' HOUR_SECOND);
+
+SELECT * FROM employees
+WHERE emp_no = 10001;
+
+UPDATE employees
+SET birth_date = DATE_ADD(birth_date, INTERVAL '2-1' YEAR_MONTH)
+WHERE emp_no = 10001;
+
+
+SELECT 
+CURDATE(), 
+SYSDATE(),
+WEEKDAY(NOW())    -- 월(0) 화(1) 수(2) 목(3) 금(4) 토(5) 일(6)
+,DAYNAME(NOW()),
+LAST_DAY(NOW()) -- 그 달의 마지막일
+,LAST_DAY('2023-06-11'),
+DATE_SUB(NOW(), INTERVAL '2' YEAR),
+EXTRACT(DAY FROM NOW()),
+DATEDIFF('2019-09-03' , '2019-06-21')
+;
